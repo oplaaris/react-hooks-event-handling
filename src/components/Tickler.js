026e6d72a8f3ceecdc5092bb53d4f92ffbadd293
Victor Leina Ngeny <victorleinangeny@gmail.com>
2023-04-21 17:39:23 +0300

    lab attempted

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 function Tickler() {
+  
   function tickle() {
     console.log("Teehee!");
   }
-
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
+  
 }
 
 export default Tickler;
